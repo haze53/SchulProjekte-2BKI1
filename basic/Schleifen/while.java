ddea0c817979a3d7786a4d18f292bb5126ef5a71
Elias <144276008+haze53@users.noreply.github.com>
2025-11-20 11:43:44 +0100

    addet while funtion

---
+++ basic/Schleifen/while.java
@@ -0,0 +1,14 @@
+public class while {
+    public static void main(String[] args) {
+
+        //while-Schleife
+        int zaehler = 1;
+
+        while(zaehler <= 5) {
+            System.out.println("Dies ist eine while-Schleife.");
+            zaehler = zaehler +1;
+      }
+
+
+  }
+}
